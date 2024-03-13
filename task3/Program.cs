9e629a621c67cb9f2a4921aa369deb06ffba8fb5
alexidi <alex.vyrlan@gmail.com>
2024-03-13 10:06:25 +0200

    Add code sort revers array

---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+Console.Clear();
+Console.WriteLine("Введите размер массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] array = Random_array(new int[n]);
+
+int[] Random_array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 20);
+    }
+    return array;
+}
+
+void Print_array(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+    Print_array(array);
+    Console.WriteLine();
+
+void ElementEndStart(int[] Array, int n)
+{
+    if (n > 0)
+    {
+        Console.Write(Array[n] + " ");
+        ElementEndStart(Array, n - 1);
+    }
+    else Console.WriteLine(Array[0]);
+}
+{
+    int[] num = array;
+    ElementEndStart(num, num.Length - 1);
+}
